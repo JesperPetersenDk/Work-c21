921b82d644360ba9ed3f559196bd1f2390bd95e6
Jesper Petersen <info@ordklar.dk>
2021-05-14 18:36:32 +0200

    Initial commit

---
+++ c-21.xUnitTesting/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace c_21.xUnitTesting
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
